a34ba4f7a6641084254eb3e739c35bf751597b08
Sebastian Mediza <sebamediza@hotmail.com>
2022-09-21 19:30:26 -0300

    Mas_cosas

--- Clase 15/script.js
+++ Clase 15/script.js
@@ -1,14 +1,39 @@
-let edad =0;
-
-let otraEdad=edad++;
-let edad2=++edad;
-
-function Sumar(x){
-    document.getElementById(contador)
-    return x+1;
+function Sumar(){
+    const lableContador = document.getElementById('contador');
+    let valorDelLableContador = lableContador.innerHTML;
+    parseInt(valorDelLableContador++);
+    lableContador.innerHTML=valorDelLableContador;
+    imprimirLable(valorDelLableContador);
 }
 
-function Restar(x){
-    document.getElementById(contador)
-    return x-1;
+function Restar(){
+    const lableContador = document.getElementById('contador');
+    let valorDelLableContador = lableContador.innerHTML;
+    parseInt(valorDelLableContador--);
+    lableContador.innerHTML=valorDelLableContador;
+    imprimirLable(valorDelLableContador); 
+}
+
+function getLable(){
+    const lableContador = document.getElementById('contador');
+    let valorDelLableContador = lableContador.innerHTML;
+    return valorDelLableContador
+}
+
+function imprimirLable(valorDelLableContador){
+    const lableParidad = document.getElementById('paridad');
+    if(!esPar(valorDelLableContador)){
+        lableParidad.innerHTML = 'Par';
+    }else{
+        lableParidad.innerHTML = 'Impar';
+    }
+}
+
+function actualizarTexto(id, nuevoValor){
+    const lableParidad = document.getElementById(id);
+    lableParidad.innerHTML = nuevoValor;
+}
+
+function esPar(valorDelLableContador){
+    return valorDelLableContador % 2;
 }
