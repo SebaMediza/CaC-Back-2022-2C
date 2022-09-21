81e7a3a1ecc70b44763800b0e027e0f445d63234
Sebastian Mediza <sebamediza@hotmail.com>
2022-09-21 18:17:57 -0300

    Mas funciones

---
+++ Clase 15/script.js
@@ -0,0 +1,14 @@
+let edad =0;
+
+let otraEdad=edad++;
+let edad2=++edad;
+
+function Sumar(x){
+    document.getElementById(contador)
+    return x+1;
+}
+
+function Restar(x){
+    document.getElementById(contador)
+    return x-1;
+}
