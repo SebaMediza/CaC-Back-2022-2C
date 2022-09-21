eda3c0c5daa1986a26f8a2ef0c409e2f63722f61
Sebastian Mediza <sebamediza@hotmail.com>
2022-09-21 10:26:22 -0300

    Esto se pone lindo

--- js/Funciones.js
+++ js/Funciones.js
@@ -1,33 +1,35 @@
 /*Primera funcion*/
 function SumaNumeros(x) {
-    return x + 2;
+    return parseFloat(x) + 2;
 }
 
 function XmasY(x,y){
-    return x + y;
+    return parseInt(x) + parseInt(y);
 }
 
 function calcular(){
     let valor1=prompt('Ingrese el valor1');
-    let valor2=prompt('Ingrese el valor2');
     let op=prompt('Ingrese la operacion(+,-,*,/)');
+    let valor2=prompt('Ingrese el valor2');
     let result;
 
     switch(op){
         case '+':
             result=parseInt(valor1) + parseInt(valor2);
             console.log(result);
+            break;
         case '-':
             result=parseInt(valor1) - parseInt(valor2);
             console.log(result);
+            break;
         case '/':
             result=parseInt(valor1) / parseInt(valor2);
             console.log(result);
+            break;
         case '*':
             result=parseInt(valor1) * parseInt(valor2);
             console.log(result);
-
-
+            break;
     }
 
 }
