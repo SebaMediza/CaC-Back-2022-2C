0cdb9b19eff159ae8ef7031cb207d608417cdee8
Sebastian Mediza <sebamediza@hotmail.com>
2022-09-26 18:08:33 -0300

    Algo

--- Clase 15/script.js
+++ Clase 15/script.js
@@ -37,7 +37,3 @@ function actualizarTexto(id, nuevoValor){
 function esPar(valorDelLableContador){
     return valorDelLableContador % 2;
 }
-
-function test(){
-    console.log(parseInt(Math.pow(-8,3))); 
-}
